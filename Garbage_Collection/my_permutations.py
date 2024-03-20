a7d87dbb0e57ac666782234d995509b1e487025b
HabtamuFeyera <habtamufeyer02@gmail.com>
2024-03-20 10:50:09 +0300

    add new file

---
+++ Garbage_Collection/my_permutations.py
@@ -0,0 +1,8 @@
+from itertools import permutations
+
+perm = permutations([1, 2, 3])
+print(list(perm))
+
+# optional: the length of the permutation tuples
+perm = permutations([1, 2, 3], 2)
+print(list(perm))
