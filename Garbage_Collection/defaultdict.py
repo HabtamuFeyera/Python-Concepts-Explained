3233e71de9c5a8a89589c26a6e7c95562c81004f
HabtamuFeyera <habtamufeyer02@gmail.com>
2024-03-20 10:32:00 +0300

    addition of the new files

---
+++ Garbage_Collection/defaultdict.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+
+# initialize with a default integer value, i.e 0
+d = defaultdict(int)
+d['one'] = 1
+d['two'] = 2
+print(d.items())
+print(d['green'])
+
+# initialize with a default list value, i.e an empty list
+d = defaultdict(list)
+s = [('yellow', 1), ('blue', 2), ('yellow', 3), ('blue', 4), ('red', 5)]
+for k, v in s:
+    d[k].append(v)
+
+print(d.items())
+print(d['green'])
