09525190b5a4cf17142b8a84fb4be64f00c56c49
HabtamuFeyera <habtamufeyer02@gmail.com>
2024-03-20 11:03:31 +0300

    add new file

--- Garbage_Collection/Lambda_Function/lamda_function.py
+++ Garbage_Collection/Lambda_Function/lamda_function.py
@@ -1,11 +1,11 @@
 # a lambda function that adds 10 to the input argument
 f = lambda x: x+100
-val1 = f(5)
-val2 = f(100)
-print(val1, val2)
+value1 = f(5)
+value2 = f(100)
+print(value1, value2)
 
 # a lambda function that multiplies two input arguments and returns the result
 f = lambda x,y: x*y
-val3 = f(2,10)
-val4 = f(7,5)
-print(val3, val4)
+value3 = f(2,10)
+value4 = f(7,5)
+print(value3, value4)
